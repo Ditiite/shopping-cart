786341a8740b67559522c92159a5db2e1ce46e25
Dita <Ditiite@users.noreply.github.com>
2017-12-09 23:55:40 +0200

    Created Shopping cart basic.

---
+++ js/index.js
@@ -0,0 +1,105 @@
+
+
+//Changind amount of items
+$('document').ready(function () {
+    var products = []; // { price, count }
+    var totalPriceLabel = document.querySelector(".total-price > .price");
+    var totalAmountLabel = document.querySelector(".total-items > .amount");
+    var mailOptionEl = document.querySelector('#mail-option');
+
+    // Initialize each product
+    $('.product').each(function () {
+        var productRow = this;
+        
+        var countEl = productRow.querySelector(".count");
+        var minusBtn = productRow.querySelector(".minus");
+        var plusBtn = productRow.querySelector(".plus");
+
+        var priceLabel =  productRow.querySelector(".sum > .price");
+        
+        // Create a new product
+        var product = {
+            index: products.length,
+            unitPrice: Number(productRow.dataset.unitPrice),
+            count: 1,
+            price: null
+        };
+
+        calculatePrice();
+        products.push(product);
+        updateElements();
+        
+        //If press minus button substract 1 item
+        $(minusBtn).click(function minus() {
+           if (product.count > 1) {
+                product.count--;
+                calculatePrice();
+                updateElements();
+                updateSummary();
+            } 
+        })
+        
+        //If press pluss button add 1 item
+        $(plusBtn).click(function plus() {
+            product.count++;
+            calculatePrice();
+            updateElements();
+            updateSummary();
+        });
+
+        // upate html entities to corresponding values
+        function updateElements() {
+            countEl.value = product.count;
+            priceLabel.innerHTML = formatPrice(product.price);
+        }
+
+        function calculatePrice() {
+            product.price = product.unitPrice * product.count;
+        }
+    });
+
+    // Run once for the first time. Also should run when product changes
+    updateSummary();
+
+    $(mailOptionEl).on('change', function () {
+        updateSummary();
+    });
+
+    function updateSummary() {
+        var summary = getTotalSummary(products);
+
+        summary.total = summary.total + Number(mailOptionEl.value);
+        totalPriceLabel.innerHTML = formatPrice(summary.total);
+        totalAmountLabel.innerHTML = summary.amount;
+    }
+
+    /**
+     * 1.234455 => 1.23
+     * 1.2566 => 1.26 OR 1.2556 =>  1.25
+     * @param {number} price 
+     */
+    function formatPrice(price) {
+        price = price.toFixed(2);
+        return price;
+    }
+});
+
+/**
+ * 
+ * @param {Array} products  List of products
+ * @return {Object} <total,amount> Summary of the cart
+ */
+function getTotalSummary(products) { 
+    var total = 0;
+    var amount = 0;
+
+    products.forEach(function(product) {
+        total = total + product.price;
+        amount = amount + product.count
+    });
+
+    return {
+        total: total,
+        amount: amount
+    }
+}
